d833a9b657b363465be36773978bf4557b36e3dc
Wenjie Xi <35672492+Jackxiini@users.noreply.github.com>
2023-09-06 18:03:20 -0400

    Update train_lbsimtsc.py

--- train_lbsimtsc.py
+++ train_lbsimtsc.py
@@ -1,10 +1,8 @@
 import os
 import argparse
 import time
-
 import numpy as np
 import torch
-
 from src.utils import read_dataset_from_npy, Logger, read_dtw_mt_from_txt
 from src.simtsc.model import SimTSC, SimTSCTrainer
 
