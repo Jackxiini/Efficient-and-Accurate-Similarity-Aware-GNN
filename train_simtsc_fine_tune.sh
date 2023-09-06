22f157afa943ebbe79ea78ea42340817a2d8a3fc
Wenjie Xi <35672492+Jackxiini@users.noreply.github.com>
2023-09-06 16:38:27 -0400

    Delete train_simtsc_fine_tune.sh

--- train_simtsc_fine_tune.sh
+++
@@ -1,6 +0,0 @@
-if_lb=1
-for i in {'OliveOil','Wine','Car','ACSF1','ShapeletSim','PigAirwayPressure','Fish','Adiac','SemgHandMovementCh2','FiftyWords','UWaveGestureLibraryY','TwoPatterns'}
-    do
-        python train_simtsc.py --dataset ${i[@]} --shot 10 --epochs 200 --lb $if_lb --K 5 --alpha 1
-        python train_simtsc.py --dataset ${i[@]} --shot 20 --epochs 200 --lb $if_lb --K 5 --alpha 1
-    done
